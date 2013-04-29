6a01a1bd4d8450de3211df8e0e8239664d88b51d
Mark Ott <mark.ott@live.com>
2013-04-28 20:51:59 -0500

    fix for failing build script

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -22,3 +22,9 @@ echo ---------------------------------------------------------------------------
 mkdir "$PROJECT_DIRECTORY/test-results"
 cd "$PROJECT_DIRECTORY"
 mocha --recursive --reporter xunit > "$PROJECT_DIRECTORY/test-results/node-unit-tests.xml"
+
+echo -----------------------------------------------------------------------------
+echo something is putting junk warning messages in my mocha output
+echo for now, just get rid of it, later need to find why this happens
+echo -----------------------------------------------------------------------------
+find . -type f -exec sed -i 's/\[Error: Symbol kerberos_module not found.\]//g' {} \;
